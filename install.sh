923db115e14acf7e4adc134fed0d7789c56a5187
haludeis <gregory.muller@debitux.com>
2015-04-15 07:38:29 +0200

    add temp_dirs/undodir

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/fswap
+mkdir -p ~/.vim/temp_dirs/undodir
 git clone https://github.com/gmarik/Vundle.vim.git  ~/.vim/bundle/Vundle.vim
 cp ~/.vim/.vimrc ~/.vimrc
 vim +PluginInstall +qall
