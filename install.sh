b08e08f8daaa8530a9d651ece7b312852ff7d7ae
haludeis <gregory.muller@debitux.com>
2015-04-15 07:34:55 +0200

    add fswap directory

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/fswap
 git clone https://github.com/gmarik/Vundle.vim.git  ~/.vim/bundle/Vundle.vim
 cp ~/.vim/.vimrc ~/.vimrc
 vim +PluginInstall +qall
